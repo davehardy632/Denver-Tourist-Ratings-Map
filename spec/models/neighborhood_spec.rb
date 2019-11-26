e0515c1666200cacbb2297b94a8e28e82f73baed
David Hardy <davehardy632@gmail.com>
2019-11-26 15:24:09 -0700

    Updating models, associating listings with neighborhoods, removing unnecessary neighborhood coordinates table.

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -3,5 +3,10 @@ require 'rails_helper'
 describe Neighborhood, type: :model do
   describe "Validations" do
     it {should validate_presence_of :name}
+    it {should validate_presence_of :coordinates}
   end
+
+  # describe "Relationships" do
+  #   it {should have_many :listings}
+  # end
 end
