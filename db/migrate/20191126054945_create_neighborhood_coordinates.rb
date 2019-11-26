a2af55939624754c327875a1d1e95f3cebde1f3e
David Hardy <davehardy632@gmail.com>
2019-11-25 22:59:43 -0700

    Updating the neighborhood coordinate table to only contain neighborhood and coordinates columns.

--- db/migrate/20191126054945_create_neighborhood_coordinates.rb
+++ db/migrate/20191126054945_create_neighborhood_coordinates.rb
@@ -1,9 +1,8 @@
 class CreateNeighborhoodCoordinates < ActiveRecord::Migration[5.2]
   def change
     create_table :neighborhood_coordinates do |t|
-      t.string :geometry_type
-      t.text :coordinates
       t.string :neighborhood
+      t.text :coordinates
     end
   end
 end
