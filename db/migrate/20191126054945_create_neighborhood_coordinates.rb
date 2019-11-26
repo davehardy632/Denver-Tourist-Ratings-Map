1005fdf560768c9ea85a370104da52a44a227575
David Hardy <davehardy632@gmail.com>
2019-11-26 15:25:33 -0700

    Implementing new migrations to handle relationships between listings and neighborhoods.

--- db/migrate/20191126054945_create_neighborhood_coordinates.rb
+++
@@ -1,8 +0,0 @@
-class CreateNeighborhoodCoordinates < ActiveRecord::Migration[5.2]
-  def change
-    create_table :neighborhood_coordinates do |t|
-      t.string :neighborhood
-      t.text :coordinates
-    end
-  end
-end
--- db/migrate/20191126180343_create_neighborhoods.rb
+++ db/migrate/20191126180343_create_neighborhoods.rb
@@ -2,6 +2,7 @@ class CreateNeighborhoods < ActiveRecord::Migration[5.2]
   def change
     create_table :neighborhoods do |t|
       t.string :name
+      t.text :coordinates
     end
   end
 end
